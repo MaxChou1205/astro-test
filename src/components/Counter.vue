7f49263ee7d10ebd7f794c62c70dbfddf4b486bd
Max <v2asault@yahoo.com.tw>
2022-08-20 11:03:57 +0800

    Commit made from VS Code Azure Static Web Apps

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        data() {
+            return {
+              count: 0,
+                test:{}
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            },
+            decrement() {
+                this.count--
+            }
+  },
+       async mounted() {
+const f = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+const data = await f.json()
+  console.log(data)
+this.test = data
+        }
+    }
+</script>
+
+<template>
+  <button @click="count++">{{count}}</button>
+  <div>
+    <label for="">id</label>
+    <input v-model="test.id" />
+
+     <label for="">title</label>
+    <input v-model="test.title" />
+  </div>
+</template>
